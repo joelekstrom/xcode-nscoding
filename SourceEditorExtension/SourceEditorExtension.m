b54a0d17f16e0be1657fe3cdd2d26e413a35996d
Joel Ekström <joel@ekstrom.io>
2017-01-03 09:52:53 +0100

    Improve menu titles

--- SourceEditorExtension/SourceEditorExtension.m
+++ SourceEditorExtension/SourceEditorExtension.m
@@ -10,18 +10,4 @@
 
 @implementation SourceEditorExtension
 
-
-- (void)extensionDidFinishLaunching
-{
-    NSLog(@"Extension did load");
-}
-
-/*
-- (NSArray <NSDictionary <XCSourceEditorCommandDefinitionKey, id> *> *)commandDefinitions
-{
-    // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
-    return @[@{XCSourceEditorCommandNameKey: @"helur?", XCSourceEditorCommandIdentifierKey: @"derp", XCSourceEditorCommandClassNameKey: @"SourceEditorCommand"}];
-}
-*/
-
 @end
